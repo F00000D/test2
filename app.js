c2732cb2b301c13951bb2dc3bc80af91ea7490d3
Winnie <winnie20050317@gmail.com>
2026-02-03 22:49:23 +0800

    add app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 let stores = []
+let likedStores = JSON.parse(localStorage.getItem("likedStores")) || []
 
 fetch("http://localhost:3000/stores")
   .then(res => res.json())
@@ -7,7 +8,7 @@ fetch("http://localhost:3000/stores")
     render()
   })
   .catch(err => {
-    console.error("開啟失敗", err)
+    console.error("後端連接失敗", err)
   })
 
 function render() {
@@ -16,25 +17,41 @@ function render() {
   container.innerHTML = ""
 
   stores
-    .filter(s => s.name.includes(keyword) || s.type.includes(keyword))
-    .forEach(store => {
-      const card = document.createElement("div")
-      card.className = "card"
+  .sort((a,b) => (likedStores.includes(b.id) ? 1 : 0) - (likedStores.includes(a.id) ? 1 : 0))
+  .filter(s => s.name.includes(keyword) || s.type.includes(keyword))
+  .forEach(store => {
+    const card = document.createElement("div")
+    card.className = "card"
 
-      card.onclick = () => {
-        window.location.href = `detail.html?id=${store.id}`
+    card.onclick = () => {
+      window.location.href = `detail.html?id=${store.id}`
+    }
+
+    const isLiked = likedStores.includes(store.id)
+
+    card.innerHTML = `
+      <div class="heart">${isLiked ? "❤️" : "🤍"}</div>
+      <img src="${store.image}">
+      <div class="card-info">
+        <h3>${store.name}</h3>
+        <p>${store.type}</p>
+      </div>
+    `
+
+    card.querySelector(".heart").onclick = e => {
+      e.stopPropagation()
+      if (likedStores.includes(store.id)) {
+        likedStores = likedStores.filter(id => id !== store.id)
+      } else {
+        likedStores.push(store.id)
       }
+      localStorage.setItem("likedStores", JSON.stringify(likedStores))
+      render() // render 之後會自動排序到最前
+    }
 
-      card.innerHTML = `
-        <img src="${store.image}">
-        <div class="card-info">
-          <h3>${store.name}</h3>
-          <p>${store.type}</p>
-        </div>
-      `
+    container.appendChild(card)
+  })
 
-      container.appendChild(card)
-    })
 }
 
 document.getElementById("search").addEventListener("input", render)
